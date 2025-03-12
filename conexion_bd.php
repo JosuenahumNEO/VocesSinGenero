648c5eab7b8b80bdc3d8712f84ffd17afed27583
JosuenahumNEO <jnla8326@gmail.com>
2025-03-12 17:37:18 -0600

    web funcional pero sin diseño propio

--- conexion_bd.php
+++ conexion_bd.php
@@ -6,7 +6,6 @@ $dbname = 'vsg';     // El nombre de la base de datos
 $user = 'root';      // El usuario de la base de datos (por defecto es 'root' en XAMPP)
 $password = '';      // La contraseña de la base de datos (por defecto está vacía en XAMPP)
 $port = 3306;        // El puerto de la base de datos
-
 try {
     $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     // Configurar el manejo de errores
--- registro_usuario.php
+++ registro_usuario.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,25 +11,25 @@
     <form action="" method="POST" class="formulario" id="formularioRegistro">
         <h2>REGISTRAR</h2>
         <?php
-        include("conexion_bd.php");
-        include("controlador_registrar_ususario.php");
+        include(__DIR__ . '/conexion_bd.php');
+        include(__DIR__ . '/controlador_registrar_usuario.php');
         ?>
         <div class="padre">
             <div class="nombre">
                 <label for="nombre">Nombre:</label>
-                <input type="text" name="nombre" id="nombre" autocomplete="off">
+                <input type="text" name="nombre" id="nombre" autocomplete="off"> 
             </div>
             <div class="correo">
                 <label for="correo">Email:</label>
                 <input type="email" name="correo" id="correo" autocomplete="off">
             </div>
-            <div class="contraseña">
-                <label for="contraseña">Contraseña</label>
-                <input type="password" name="contraseña" id="contraseña" autocomplete="off">
+            <div class="contrasena">
+                <label for="contrasena">Contraseña</label>
+                <input type="password" name="contrasena" id="contrasena" autocomplete="off" minlength="8">
             </div>
-            <div class="confirmarcontraseña">
-                <label for="confirmarcontraseña">Confirmar Contraseña</label>
-                <input type="password" name="confirmarcontraseña" id="confirmarcontraseña" autocomplete="off">
+            <div class="confirmarcontrasena">
+                <label for="confirmarcontrasena">Confirmar Contraseña</label>
+                <input type="password" name="confirmarcontrasena" id="confirmarcontrasena" autocomplete="off" minlength="8">
             </div>
             <button class="button" type="submit" name="registro">Registrar</button>
         </div>
@@ -55,14 +55,14 @@
     });
 
     document.getElementById("correo").addEventListener("keydown", function (event) {
-        manejarEnter(event, "contraseña");
+        manejarEnter(event, "contrasena");
     });
 
-    document.getElementById("contraseña").addEventListener("keydown", function (event) {
-        manejarEnter(event, "confirmarcontraseña");
+    document.getElementById("contrasena").addEventListener("keydown", function (event) {
+        manejarEnter(event, "confirmarcontrasena");
     });
 
-    document.getElementById("confirmarcontraseña").addEventListener("keydown", function (event) {
+    document.getElementById("confirmarcontrasena").addEventListener("keydown", function (event) {
         manejarEnter(event, null); // Envía el formulario al presionar Enter en el último campo
     });
 </script>

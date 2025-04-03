98a37b302f2de27a1f2b509ae0d9566da48d762a
JosuenahumNEO <jlopez183@ucol.mx>
2025-04-03 10:42:26 -0600

    archivo despues del cambio de boton de login

--- conexion_bd.php
+++ conexion_bd.php
@@ -1,18 +1,15 @@
 <?php
-// modelo/conexion_bd.php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "vsg";
 
-$host = 'localhost'; // El servidor de la base de datos
-$dbname = 'vsg';     // El nombre de la base de datos
-$user = 'root';      // El usuario de la base de datos (por defecto es 'root' en XAMPP)
-$password = '';      // La contraseña de la base de datos (por defecto está vacía en XAMPP)
-$port = 3306;        // El puerto de la base de datos
-try {
-    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
-    // Configurar el manejo de errores
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Configurar el juego de caracteres a UTF-8
-    $conexion->exec("SET NAMES 'utf8'");
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+$conexion = mysqli_connect($host, $user, $password, $dbname);
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
+
+// Configurar caracteres a UTF-8
+mysqli_set_charset($conexion, "utf8");
 ?>

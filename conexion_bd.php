5ec0962b5cb4f1d1528473be8a618ba038e037f5
Tonatiuh-softdev <rtonapreciado@gmail.com>
2025-05-09 14:41:54 -0600

    login msqli de josue

--- conexion_bd.php
+++ conexion_bd.php
@@ -3,16 +3,18 @@
 
 $host = 'localhost'; // El servidor de la base de datos
 $dbname = 'vsg';     // El nombre de la base de datos
-$user = 'root';      // El usuario de la base de datos (por defecto es 'root' en XAMPP)
-$password = '';      // La contraseña de la base de datos (por defecto está vacía en XAMPP)
+$user = 'root';      // El usuario de la base de datos
+$password = '';      // La contraseña de la base de datos
 $port = 3306;        // El puerto de la base de datos
-try {
-    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
-    // Configurar el manejo de errores
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Configurar el juego de caracteres a UTF-8
-    $conexion->exec("SET NAMES 'utf8'");
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+
+// Crear conexión
+$conexion = new mysqli($host, $user, $password, $dbname, $port);
+
+// Verificar conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
 }
+
+// Establecer el conjunto de caracteres a UTF-8
+$conexion->set_charset("utf8");
 ?>
